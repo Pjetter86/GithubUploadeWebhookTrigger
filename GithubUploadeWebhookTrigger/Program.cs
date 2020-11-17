e4bc97366e8e97933208654c3294c9fc0d359889
Peter Børlum <pa_eman@hotmail.com>
2020-11-17 14:33:16 +0100

    First

--- GithubUploadeWebhookTrigger/Program.cs
+++ GithubUploadeWebhookTrigger/Program.cs
@@ -6,7 +6,7 @@ namespace GithubUploadeWebhookTrigger
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string test = "test";
         }
     }
 }
