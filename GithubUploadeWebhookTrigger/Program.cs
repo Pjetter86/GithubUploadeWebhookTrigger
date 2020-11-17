1fdc1dad24c94798b31845717d907371b78e09f0
Peter Børlum <pa_eman@hotmail.com>
2020-11-17 15:33:20 +0100

    third

--- GithubUploadeWebhookTrigger/Program.cs
+++ GithubUploadeWebhookTrigger/Program.cs
@@ -6,7 +6,7 @@ namespace GithubUploadeWebhookTrigger
     {
         static void Main(string[] args)
         {
-            string test = "test2";
+            string test = "test3";
         }
     }
 }
